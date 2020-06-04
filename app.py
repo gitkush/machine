71f1597493f0695fdbb62b1e3d181844d0e3a8bc
gitkush <38980954+gitkush@users.noreply.github.com>
2020-06-04 11:41:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,99 +1,133 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pydeck as pdk
-import plotly.express as px
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.metrics import precision_score, recall_score 
 
+def main():
+    st.title("Binary Classification Web App")
+    st.sidebar.title("Binary Classification Web App")
+    st.sidebar.markdown("This goes below the text")
+    @st.cache(persist=True)
+    def load_data():
+    	data = pd.read_csv("mushrooms.csv")
+    	label = LabelEncoder()
+    	for col in data.columns:
+    		data[col] = label.fit_transform(data[col])
+    	return data
 
-
-DATA_URL = "Data.csv"
-
-
-st.title("Motor Vehicle Collisions n New York")
-
-st.markdown("This application is a streamlit dashboard ")
-
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL,nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
-    data.dropna(subset=['LATITUDE','LONGITUDE'], inplace=True)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data.rename(columns={'crash_date_crash_time':'date/time'}, inplace = True)
-    return data
-
-
-data = load_data(100000)
-original_data = data
-
-
-st.header("Where are the most people injured in NYC")
-injured_people = st.slider("Number of persons injured in vehice collisions:",0,19)
-st.map(data.query("injured_persons >= @injured_people")[["latitude","longitude"]].dropna(how="any"))
-
-
-st.header("How many collisions occured during a given time of day")
-hour = st.slider("Hour to look at:", 0,23)
-data = data[data["date/time"].dt.hour == hour]
-
-st.markdown("Vehicle collisions between %i:00 and %i:00" %(hour, (hour+1) %24))
-
-midpoint = (np.average(data['latitude']), np.average(data['longitude']))
-
-st.write(pdk.Deck(
-        map_style="mapbox://styles/mapbox/light-v9",
-        initial_view_state = {
-            "latitude" : midpoint[0],
-            "longitude" : midpoint[1],
-            "zoom":11,
-            "pitch":50
-        },
-        layers = [
-            pdk.Layer(
-                "HexagonLayer",
-                data = data[['date/time','latitude', 'longitude']],
-                get_position = ['longitude', 'latitude'],
-                radius = 100,
-                extruded = True,
-                pickable = True,
-                elevation_scale = 4,
-                elevation_range = [0,1000]
-            )
-        ]
-))
-
-
-st.subheader("Breakdown by minute between %i:00 and %i:00" %(hour, (hour + 1) %24))
-filtered = data[
-    (data['date/time'].dt.hour >= hour) & (data["date/time"].dt.hour < (hour + 1))
-]
-
-st.write(filtered)
-
-hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0,60))[0]
-chart_data = pd.DataFrame({'minute': range(60), 'crashes':hist})
-
-# st.write(chart_data)
-
-fig = px.bar(chart_data, x='minute', y='crashes', hover_data = ['minute', 'crashes'], height = 400)
-st.write(fig)
-
-
-st.header("Top 5 dangerous street by affected type")
-select = st.selectbox("Affected type of people",["Pedestrians","Cyclists","Motorists"])
-
-if select == "Pedestrians":
-    st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending = False).dropna(how='any')[:5])
-
-elif select == "Cyclists":
-    st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].sort_values(by=['injured_cyclists'], ascending = False).dropna(how='any')[:5])
+    @st.cache(persist=True)
+    def split(df):
+    	y = df.type
+    	x = df.drop(columns=['type'])
+    	x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.3, random_state = 0)
+    	return x_train, x_test, y_train, y_test
     
-else:
-    st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(by=['injured_motorists'], ascending = False).dropna(how='any')[:5])
+    def plot_metrics(metrics_list):
+    	if 'Confusion Matrix' in metrics_list:
+    		st.subheader('Confusion Matrix')
+    		plot_confusion_matrix(model, x_test, y_test, display_labels=class_names)
+    		st.pyplot()
+
+    	if 'ROC Curve' in metrics_list:
+    		st.subheader('ROC Curve')
+    		plot_roc_curve(model, x_test, y_test)
+    		st.pyplot()
+
+    	if 'Precision Recall Curve' in metrics_list:
+    		st.subheader('Precision Recall Curve')
+    		plot_precision_recall_curve(model, x_test, y_test)
+    		st.pyplot()
 
 
-if st.checkbox("Show raw data", False):
-    st.subheader('Raw Data')
-    st.write(data)
+    df = load_data()
+    x_train, x_test, y_train, y_test = split(df)
+    class_names = ['edible','poisnous']
+    st.sidebar.subheader("Choose subheader")
+    classifier = st.sidebar.selectbox("Classifier", ("Support Vector Machine (SVM)","Logistic Regression","Random Forest"))
+
+    if classifier == "Support Vector Machine (SVM)":
+    	st.sidebar.subheader("Model Hyperparameters")
+    	C = st.sidebar.number_input("C (Regularization Parameter)", 0.01, 10.0, step = 0.01, key='C')
+    	kernel = st.sidebar.radio("kernel",("rbf", "linear"), key = "kernel")
+    	gamma = st.sidebar.radio("Gamma (Kernel Coefficient)",("scale", "auto"), key = "gamma")
+
+    	metrics = st.sidebar.multiselect("What matrix to plot", ("Confusion Matrix", "ROC Curve", "Precision Recall Curve"))
+
+    	if st.sidebar.button("Classify", key = "classify"):
+    		st.subheader("Support Vector Machine (SVM) Results")
+    		model = SVC(C = C, kernel = kernel, gamma = gamma)
+    		model.fit(x_train,y_train)
+    		accuracy = model.score(x_test, y_test)
+    		y_pred = model.predict(x_test)
+    		st.write("Accuracy: ", accuracy.round(2))
+    		st.write("Precision: ", precision_score(y_test, y_pred, labels = class_names).round(2))
+    		st.write("Recall: ", recall_score(y_test, y_pred, labels = class_names).round(2))
+    		plot_metrics(metrics)
+
+    if classifier == "Logistic Regression":
+    	st.sidebar.subheader("Model Hyperparameters")
+    	C = st.sidebar.number_input("C (Regularization Parameter)", 0.01, 10.0, step = 0.01, key='C_LR')
+
+    	max_iter = st.sidebar.slider("Maximum number of iterations",100, 500, key = "max_iter")
+
+
+    	metrics = st.sidebar.multiselect("What matrix to plot", ("Confusion Matrix", "ROC Curve", "Precision Recall Curve"))
+
+    	if st.sidebar.button("Classify", key = "classify"):
+    		st.subheader("Logistic Regression Results")
+    		model = LogisticRegression(C = C, max_iter = max_iter)
+    		model.fit(x_train,y_train)
+    		accuracy = model.score(x_test, y_test)
+    		y_pred = model.predict(x_test)
+    		st.write("Accuracy: ", accuracy.round(2))
+    		st.write("Precision: ", precision_score(y_test, y_pred, labels = class_names).round(2))
+    		st.write("Recall: ", recall_score(y_test, y_pred, labels = class_names).round(2))
+    		plot_metrics(metrics)
+
+    if classifier == "Random Forest":
+    	st.sidebar.subheader("Model Hyperparameters")
+    	n_estimators = st.sidebar.number_input("The number of trees in the forest", 100, 5000, step = 10, key = 'n_estimators')
+    	max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 20, step = 1, key = 'max_depth')
+    	bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ("True","False"), key="boostrap")
+
+    	metrics = st.sidebar.multiselect("What matrix to plot", ("Confusion Matrix", "ROC Curve", "Precision Recall Curve"))
+
+    	if st.sidebar.button("Classify", key = "classify"):
+    		st.subheader("Random Forest Results")
+    		model = RandomForestClassifier(n_estimators = n_estimators, max_depth = max_depth, bootstrap = bootstrap, n_jobs = -1)
+    		model.fit(x_train,y_train)
+    		accuracy = model.score(x_test, y_test)
+    		y_pred = model.predict(x_test)
+    		st.write("Accuracy: ", accuracy.round(2))
+    		st.write("Precision: ", precision_score(y_test, y_pred, labels = class_names).round(2))
+    		st.write("Recall: ", recall_score(y_test, y_pred, labels = class_names).round(2))
+    		plot_metrics(metrics)
+
+
+
+
+
+
+
+
+
+
+
+    if st.sidebar.checkbox("Show raw data",False):
+    	st.subheader("Mushroom Dataset (Classification)")
+    	st.write(df)
+
+ 
+
+
+
+if __name__ == '__main__':
+    main()
 
 
