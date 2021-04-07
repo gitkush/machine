eef7c8428fd904feb45c832474f18c5af588fbed
Ankush Gaur <38980954+gitkush@users.noreply.github.com>
2021-04-07 17:42:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,16 @@ import re
 def main():
     st.title("Get data from Linkedⓘⓝ")
     st.sidebar.title("Provide your details here:")
+    li_at = st.sidebar.text_input('Your li_at cookie:')
 
-    if st.button('Get Data'):
+    JSESSIONID = st.sidebar.text_input('Your JSESSIONID:')
+
+    GSHEET_LINK = st.sidebar.text_input('Link to "readable" GSHEET with input:')
+    if st.sidebar.button('Get Data'):
     	get_data()
 
 
 def get_data():
-	li_at = st.sidebar.text_input('Your li_at cookie:')
-	JSESSIONID = st.sidebar.text_input('Your JSESSIONID:')
-	GSHEET_LINK = st.sidebar.text_input('Link to "readable" GSHEET with input:')
 	st.write(li_at)
 	st.write(JSESSIONID)
 	st.write(GSHEET_LINK)
