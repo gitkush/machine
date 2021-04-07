5c16177743b2954529a5cea591198f1d7b30b725
Ankush Gaur <38980954+gitkush@users.noreply.github.com>
2021-04-07 17:16:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,12 @@ import re
 
 
 def main():
-    st.title("Get data from LinkedIn ⓘⓝ")
+    st.title("Get data from Linkedⓘⓝ")
     st.sidebar.title("Provide your details here:")
     li_at = st.sidebar.text_input('Your li_at cookie:')
     st.sidebar.write(li_at)
-
+    JSESSIONID = st.sidebar.text_input('Your JSESSIONID:')
+    st.sidebar.write(JSESSIONID)
 
  #    headers = {"user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",}
 
@@ -39,145 +40,3 @@ def main():
 if __name__ == '__main__':
     main()
  
-    
-#     response = requests.get("https://www.moneycontrol.com/india/stockpricequote/miscellaneous/bselimited/B08")
-#     soup = BeautifulSoup(response.text,"html.parser")
-    
-#     val = soup.select('span.span_price_wrap.stprh.rdclr')
-#     value = re.search("([0-9]).*([0-9])",str(val))
-    
-#     st.write("NSE Value: ", value.group(0))
-    
-#     @st.cache(persist=True)
-#     def load_data():
-#     	data = pd.read_csv("mushrooms.csv")
-#     	label = LabelEncoder()
-#     	for col in data.columns:
-#     		data[col] = label.fit_transform(data[col])
-#     	return data
-
-#     @st.cache(persist=True)
-#     def split(df):
-#     	y = df.type
-#     	x = df.drop(columns=['type'])
-#     	x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.3, random_state = 0)
-#     	return x_train, x_test, y_train, y_test
-    
-#     def plot_metrics(metrics_list):
-#     	if 'Confusion Matrix' in metrics_list:
-#     		st.subheader('Confusion Matrix')
-#     		plot_confusion_matrix(model, x_test, y_test, display_labels=class_names)
-#     		st.pyplot()
-
-#     	if 'ROC Curve' in metrics_list:
-#     		st.subheader('ROC Curve')
-#     		plot_roc_curve(model, x_test, y_test)
-#     		st.pyplot()
-
-#     	if 'Precision Recall Curve' in metrics_list:
-#     		st.subheader('Precision Recall Curve')
-#     		plot_precision_recall_curve(model, x_test, y_test)
-#     		st.pyplot()
-
-
-#     df = load_data()
-#     x_train, x_test, y_train, y_test = split(df)
-#     class_names = ['edible','poisnous']
-#     st.sidebar.subheader("Choose subheader")
-#     classifier = st.sidebar.selectbox("Classifier", ("Support Vector Machine (SVM)","Logistic Regression","Random Forest"))
-
-#     if classifier == "Support Vector Machine (SVM)":
-#     	st.sidebar.subheader("Model Hyperparameters")
-#     	C = st.sidebar.number_input("C (Regularization Parameter)", 0.01, 10.0, step = 0.01, key='C')
-#     	kernel = st.sidebar.radio("kernel",("rbf", "linear"), key = "kernel")
-#     	gamma = st.sidebar.radio("Gamma (Kernel Coefficient)",("scale", "auto"), key = "gamma")
-
-#     	metrics = st.sidebar.multiselect("What matrix to plot", ("Confusion Matrix", "ROC Curve", "Precision Recall Curve"))
-
-#     	if st.sidebar.button("Classify", key = "classify"):
-#     		st.subheader("Support Vector Machine (SVM) Results")
-#     		model = SVC(C = C, kernel = kernel, gamma = gamma)
-#     		model.fit(x_train,y_train)
-#     		accuracy = model.score(x_test, y_test)
-#     		y_pred = model.predict(x_test)
-#     		st.write("Accuracy: ", accuracy.round(2))
-#     		st.write("Precision: ", precision_score(y_test, y_pred, labels = class_names).round(2))
-#     		st.write("Recall: ", recall_score(y_test, y_pred, labels = class_names).round(2))
-#     		plot_metrics(metrics)
-
-#     if classifier == "Logistic Regression":
-#     	st.sidebar.subheader("Model Hyperparameters")
-#     	C = st.sidebar.number_input("C (Regularization Parameter)", 0.01, 10.0, step = 0.01, key='C_LR')
-
-#     	max_iter = st.sidebar.slider("Maximum number of iterations",100, 500, key = "max_iter")
-
-
-#     	metrics = st.sidebar.multiselect("What matrix to plot", ("Confusion Matrix", "ROC Curve", "Precision Recall Curve"))
-
-#     	if st.sidebar.button("Classify", key = "classify"):
-#     		st.subheader("Logistic Regression Results")
-#     		model = LogisticRegression(C = C, max_iter = max_iter)
-#     		model.fit(x_train,y_train)
-#     		accuracy = model.score(x_test, y_test)
-#     		y_pred = model.predict(x_test)
-#     		st.write("Accuracy: ", accuracy.round(2))
-#     		st.write("Precision: ", precision_score(y_test, y_pred, labels = class_names).round(2))
-#     		st.write("Recall: ", recall_score(y_test, y_pred, labels = class_names).round(2))
-#     		plot_metrics(metrics)
-
-#     if classifier == "Random Forest":
-#     	st.sidebar.subheader("Model Hyperparameters")
-#     	n_estimators = st.sidebar.number_input("The number of trees in the forest", 100, 5000, step = 10, key = 'n_estimators')
-#     	max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 20, step = 1, key = 'max_depth')
-#     	bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ("True","False"), key="boostrap")
-
-#     	metrics = st.sidebar.multiselect("What matrix to plot", ("Confusion Matrix", "ROC Curve", "Precision Recall Curve"))
-
-#     	if st.sidebar.button("Classify", key = "classify"):
-#     		st.subheader("Random Forest Results")
-#     		model = RandomForestClassifier(n_estimators = n_estimators, max_depth = max_depth, bootstrap = bootstrap, n_jobs = -1)
-#     		model.fit(x_train,y_train)
-#     		accuracy = model.score(x_test, y_test)
-#     		y_pred = model.predict(x_test)
-#     		st.write("Accuracy: ", accuracy.round(2))
-#     		st.write("Precision: ", precision_score(y_test, y_pred, labels = class_names).round(2))
-#     		st.write("Recall: ", recall_score(y_test, y_pred, labels = class_names).round(2))
-#     		plot_metrics(metrics)
-
-
-
-#     if st.sidebar.checkbox("Show raw data",False):
-#     	st.subheader("Mushroom Dataset (Classification)")
-#     	st.write(df)
-
-
-
-
-
-
-
-
-
-
-
-
-# import requests
-# from bs4 import BeautifulSoup
-# import re
-
-# def soup_val():
-# #     response = requests.get("https://www.moneycontrol.com/india/stockpricequote/refineries/relianceindustries/RI")
-# #     soup = BeautifulSoup(response.text,"lxml")
-
-# #     val = soup.find_all("li", class_="market_depth_graph")[0].text
-#     val = "Testing"
-#     return(val)
-
-#     st.button("Re-run")
-
-# if st.button('Re-run'):
-#     result = soup_val()
-#     st.write(result)
-
-
-
